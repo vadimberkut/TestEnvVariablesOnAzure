964cdd7cb7f00c7f3553e7f233a136c28f3b02c6
vadimberkut <vadimberkut8@gmail.com>
2018-08-20 18:31:27 +0300

    first commit

---
+++ TestEnvVariablesOnAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestEnvVariablesOnAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
